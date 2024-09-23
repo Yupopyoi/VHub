aa5e4600380395f247520f9e44ec5e1639acf815
Yupopoi <ysy2001number1@docomo.ne.jp>
2024-09-23 21:21:35 +0900

    Rewrite the hand detection process to apply for VRM.

--- Assets/Scripts/MediaPipe/HandMark/HandLandmarkDetector.cs
+++ Assets/Scripts/MediaPipe/HandMark/HandLandmarkDetector.cs
@@ -1,16 +1,158 @@
+// Copyright (c) 2023 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// Copyright (c) 2024 Yupopyoi
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 using Mediapipe.Unity.Sample.HandLandmarkDetection;
 using Mediapipe.Unity.Sample;
+using Mediapipe.Tasks.Vision.HandLandmarker;
+using UnityEngine.Rendering;
 
 namespace Mediapipe.Unity.Yupopyoi.HandLandmark
 {
-    public class HandLandmarkDetector : HandLandmarkerRunner
+    public class HandLandmarkDetector : VisionTaskApiRunner<HandLandmarker>
     {
+        [SerializeField] private HandLandmarkerResultProvider _handLandmarkerResultProvider;
 
-        public void onChangedCameraDevice(int id = 1)
+        private Experimental.TextureFramePool _textureFramePool;
+
+        public readonly HandLandmarkDetectionConfig config = new();
+
+        public override void Stop()
+        {
+            base.Stop();
+            _textureFramePool?.Dispose();
+            _textureFramePool = null;
+        }
+
+        protected override IEnumerator Run()
+        {
+            Debug.Log($"Delegate = {config.Delegate}");
+            Debug.Log($"Running Mode = {config.RunningMode}");
+            Debug.Log($"NumHands = {config.NumHands}");
+            Debug.Log($"MinHandDetectionConfidence = {config.MinHandDetectionConfidence}");
+            Debug.Log($"MinHandPresenceConfidence = {config.MinHandPresenceConfidence}");
+            Debug.Log($"MinTrackingConfidence = {config.MinTrackingConfidence}");
+
+            yield return AssetLoader.PrepareAssetAsync(config.ModelPath);
+
+            var options = config.GetHandLandmarkerOptions(config.RunningMode == Tasks.Vision.Core.RunningMode.LIVE_STREAM ? OnHandLandmarkDetectionOutput : null);
+            taskApi = HandLandmarker.CreateFromOptions(options, GpuManager.GpuResources);
+            var imageSource = ImageSourceProvider.ImageSource;
+
+            yield return imageSource.Play();
+
+            if (!imageSource.isPrepared)
+            {
+                Debug.LogError("Failed to start ImageSource, exiting...");
+                yield break;
+            }
+
+            // Use RGBA32 as the input format.
+            // TODO: When using GpuBuffer, MediaPipe assumes that the input format is BGRA, so maybe the following code needs to be fixed.
+            _textureFramePool = new Experimental.TextureFramePool(imageSource.textureWidth, imageSource.textureHeight, TextureFormat.RGBA32, 10);
+
+            // NOTE: The screen will be resized later, keeping the aspect ratio.
+            screen.Initialize(imageSource);
+
+            SetupAnnotationController(_handLandmarkerResultProvider, imageSource);
+
+            var transformationOptions = imageSource.GetTransformationOptions();
+            var flipHorizontally = transformationOptions.flipHorizontally;
+            var flipVertically = transformationOptions.flipVertically;
+            var imageProcessingOptions = new Tasks.Vision.Core.ImageProcessingOptions(rotationDegrees: (int)transformationOptions.rotationAngle);
+
+            AsyncGPUReadbackRequest req = default;
+            var waitUntilReqDone = new WaitUntil(() => req.done);
+            var result = HandLandmarkerResult.Alloc(options.numHands);
+
+            // NOTE: we can share the GL context of the render thread with MediaPipe (for now, only on Android)
+            var canUseGpuImage = options.baseOptions.delegateCase == Tasks.Core.BaseOptions.Delegate.GPU &&
+              SystemInfo.graphicsDeviceType == GraphicsDeviceType.OpenGLES3 &&
+              GpuManager.GpuResources != null;
+            using var glContext = canUseGpuImage ? GpuManager.GetGlContext() : null;
+
+            while (true)
+            {
+                if (isPaused)
+                {
+                    yield return new WaitWhile(() => isPaused);
+                }
+
+                if (!_textureFramePool.TryGetTextureFrame(out var textureFrame))
+                {
+                    yield return new WaitForEndOfFrame();
+                    continue;
+                }
+
+                // Build the input Image
+                Image image;
+                if (canUseGpuImage)
+                {
+                    yield return new WaitForEndOfFrame();
+                    textureFrame.ReadTextureOnGPU(imageSource.GetCurrentTexture(), flipHorizontally, flipVertically);
+                    image = textureFrame.BuildGpuImage(glContext);
+                }
+                else
+                {
+                    req = textureFrame.ReadTextureAsync(imageSource.GetCurrentTexture(), flipHorizontally, flipVertically);
+                    yield return waitUntilReqDone;
+
+                    if (req.hasError)
+                    {
+                        Debug.LogError($"Failed to read texture from the image source, exiting...");
+                        break;
+                    }
+                    image = textureFrame.BuildCPUImage();
+                    textureFrame.Release();
+                }
+
+                switch (taskApi.runningMode)
+                {
+                    case Tasks.Vision.Core.RunningMode.IMAGE:
+                        if (taskApi.TryDetect(image, imageProcessingOptions, ref result))
+                        {
+                            _handLandmarkerResultProvider.DrawNow(result);
+                        }
+                        else
+                        {
+                            _handLandmarkerResultProvider.DrawNow(default);
+                        }
+                        break;
+                    case Tasks.Vision.Core.RunningMode.VIDEO:
+                        if (taskApi.TryDetectForVideo(image, GetCurrentTimestampMillisec(), imageProcessingOptions, ref result))
+                        {
+                            _handLandmarkerResultProvider.DrawNow(result);
+                        }
+                        else
+                        {
+                            _handLandmarkerResultProvider.DrawNow(default);
+                        }
+                        break;
+                    case Tasks.Vision.Core.RunningMode.LIVE_STREAM:
+                        taskApi.DetectAsync(image, GetCurrentTimestampMillisec(), imageProcessingOptions);
+                        break;
+                }
+            }
+        }
+
+        private void OnHandLandmarkDetectionOutput(HandLandmarkerResult result, Image image, long timestamp)
+        {
+            _handLandmarkerResultProvider.DrawLater(result);
+        }
+
+        public void OnChangedCameraDevice(int id = 1)
         {
             Stop();
             var imageSource = ImageSourceProvider.ImageSource;
