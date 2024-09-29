1774fa6528f1272983c8a5e1207919fc4017ce05
Yupopoi <ysy2001number1@docomo.ne.jp>
2024-09-29 15:10:20 +0900

    Enabled to calculate the shoulder tilt angle.

--- Assets/Scripts/MediaPipe/Annotation/MediaPipeAllocator.cs
+++ Assets/Scripts/MediaPipe/Annotation/MediaPipeAllocator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 using Mediapipe.Tasks.Vision.HandLandmarker;
 using Mediapipe.Tasks.Vision.PoseLandmarker;
+using System;
 
 namespace Mediapipe.Unity.Yupopyoi.Allocator
 {
@@ -11,19 +12,38 @@ namespace Mediapipe.Unity.Yupopyoi.Allocator
     {
 
         /*
+         * [Detection]
+         * 
+         * Index  Body parts    ïîà       VRM Model      Axis
+         * 
          * 11	left shoulder	ç∂å®   J_Bip_L_Shoulder   xyz
 	     * 12  right shoulder	âEå®   J_Bip_R_Shoulder   xyz
          * 
-         * 23   left hip	ç∂êK Å@_Bip_L_UpperLeg   xyz
-	     * 24  right hip	âEêK Å@_Bip_R_UpperLeg   xyz
+         * 23       left hip	ç∂êK Å@J_Bip_L_UpperLeg   xyz
+	     * 24      right hip	âEêK Å@J_Bip_R_UpperLeg   xyz
          *  
+         */
+
+        /* 
+         * [Controll]
+         * 
+         *  J_Bip_C_Spine local-x : ì∑ëÃëOå„ì|ÇµÅ@(Rotation around the frontal axis)
+         *  J_Bip_C_Spine local-y : ì∑ëÃâ°îPÇËÅ@  (Rotation around the longitudinal axis)
+         *  J_Bip_C_Spine local-z : ì∑ëÃç∂âEì|ÇµÅ@(Rotation around the sagittal axis)
+         * 
          */
 
         public void AllocatePose(PoseLandmarkerResult poseTarget)
         {
-            Debug.Log(poseTarget.poseLandmarks[0].landmarks[11].x + " / " +
-                      poseTarget.poseLandmarks[0].landmarks[11].y + " / " +
-                      poseTarget.poseLandmarks[0].landmarks[11].z);
+            //Debug.Log(poseTarget.poseLandmarks[0].landmarks[11].y + " / " + poseTarget.poseLandmarks[0].landmarks[12].y);
+
+            // Shoulder diff
+            float shoulder_xdiff = poseTarget.poseLandmarks[0].landmarks[12].x - poseTarget.poseLandmarks[0].landmarks[11].x;
+            float shoulder_ydiff = poseTarget.poseLandmarks[0].landmarks[12].y - poseTarget.poseLandmarks[0].landmarks[11].y;
+
+            double rotate_y_deg = Math.Atan((double)shoulder_ydiff / shoulder_xdiff) * 180 / Math.PI;
+
+            Debug.Log(rotate_y_deg);
         }
     }
 }// namespace Mediapipe.Unity.Yupopyoi.Allocator
