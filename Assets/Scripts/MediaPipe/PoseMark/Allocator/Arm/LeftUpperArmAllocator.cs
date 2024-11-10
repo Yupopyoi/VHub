335341fa1c1e5db4d584796f83df7ba68369e0fe
Yupopoi <ysy2001number1@docomo.ne.jp>
2024-11-10 23:28:05 +0900

    [Add] Adjustable upper arm angle from lower arm angle.

--- Assets/Scripts/MediaPipe/PoseMark/Allocator/Arm/LeftUpperArmAllocator.cs
+++ Assets/Scripts/MediaPipe/PoseMark/Allocator/Arm/LeftUpperArmAllocator.cs
@@ -5,11 +5,12 @@
 // https://opensource.org/licenses/MIT.
 
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace Mediapipe.Unity.Yupopyoi.Allocator
 {
-    public class LeftUpperArmAllocator : PoseAllocatorBase
+    public class LeftUpperArmAllocator : PoseAllocatorWithReverseBase
     {
         /* 
          *  LandmarksIndex     Body parts    •”ˆÊ
@@ -34,20 +35,49 @@ namespace Mediapipe.Unity.Yupopyoi.Allocator
 
             float parentOffsetZ = 360 - parentRotation.Value.Z;
 
-            var armRotation = Quaternion.FromToRotation(Vector3.right, direction.normalized).eulerAngles;
+            var armRotation  = Quaternion.FromToRotation(Vector3.right, direction.normalized).eulerAngles;
 
-            Debug.Log(leftElbowVector + " / " + leftShoulderVector + " / " + armRotation.y);
+            float ry = MakeNearZeroContinuous(armRotation.y + initialRotation.Y + parentRotation.Value.Y);
+            float rz = MakeNearZeroContinuous(-armRotation.z + initialRotation.Z + parentOffsetZ);
 
-            var currentLocalRotation = new LocalRotation(initialRotation.X,
-                                                         MakeNearZeroContinuous( armRotation.y + initialRotation.Y + parentRotation.Value.Y),
-                                                         MakeNearZeroContinuous(-armRotation.z + initialRotation.Z + parentOffsetZ));
+            var currentLocalRotation = new LocalRotation(initialRotation.X, ry, rz);
 
             AddCurrentLocalRotation(currentLocalRotation);
 
             UpdateBodyPartAverageLocalRotation();
             ApplyToModel();
 
+            reverseMessage = new(initialRotation.X, ry, rz);
+
             return;
+        }
+
+        // Correction by lower arm orientation
+        public override void ReverseAllocate(ReverseMessage msg)
+        {
+            float childBendAngle = Mathf.Sin(msg.AdjustmentRotation().Z * Mathf.PI / 180.0f);
+            float thisRotation_z = Mathf.Cos(reverseMessage.Rz * Mathf.PI / 180.0f);
+
+            float adjustRate = CommonUtils.Sigmoid(Mathf.Abs(childBendAngle * thisRotation_z), 10);
+
+            AddAdjustmentLocalRotation(new LocalRotation(0.0f, -60.0f * adjustRate, 0.0f));
+            UpdateAdjustmentAverageLocalRotation();
+            AdjustModel();
+            return;
+        }
+
+        protected override void AdjustModel()
+        {
+            var localAngle = bodyPart.transform.localEulerAngles;
+
+            localAngle.y += adjustmentAverageLocalRotation.Y;
+
+            if(localAngle.y > 270.0f)
+            {
+                localAngle.y = 0.0f;
+            }
+
+            bodyPart.transform.localEulerAngles = localAngle;
         }
 
     }
